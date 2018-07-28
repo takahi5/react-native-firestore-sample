f206e3356ad90f483883271d71d6505e6271f3cb
wada.takahiko <wada.takahiko@gmail.com>
2018-07-29 07:09:37 +0900

    名前フォームを追加

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { FormLabel, FormInput } from "react-native-elements";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -7,7 +8,12 @@ export default class HomeScreen extends React.Component {
   };
 
   render() {
-    return <View style={styles.container} />;
+    return (
+      <View style={styles.container}>
+        <FormLabel>名前</FormLabel>
+        <FormInput onChangeText={name => console.log(name)} />
+      </View>
+    );
   }
 }
 
