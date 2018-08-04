ac721239930283ef822cf86cbca57f8464a34070
wada.takahiko <wada.takahiko@gmail.com>
2018-08-05 02:05:16 +0900

    Firestoreにデータを保存

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,37 +1,87 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { FormLabel, FormInput } from "react-native-elements";
+import { FormLabel, FormInput, Button } from "react-native-elements";
+import Fire from "../utils/Fire";
+import firebase from "firebase";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null
   };
 
+  state = {
+    name: "",
+    cal: 0,
+    protein: 0,
+    lipid: 0,
+    carbohydrate: 0,
+    date: "2018-07-01"
+  };
+
+  async componentDidMount() {
+    // Check if we are signed in...
+    if (Fire.shared.uid) {
+      // If we are, then we can get the first 5 posts
+      const res = await Fire.shared.getFoods();
+      console.log(res);
+    } else {
+      // If we aren't then we should just start observing changes. This will be called when the user signs in
+      firebase.auth().onAuthStateChanged(async user => {
+        if (user) {
+          const res = await Fire.shared.getFoods();
+          console.log(res);
+        }
+      });
+    }
+  }
+
+  createFood() {
+    const { name, cal, protein, lipid, carbohydrate, date } = this.state;
+    Fire.shared.createFood({
+      name,
+      cal,
+      protein,
+      lipid,
+      carbohydrate,
+      date
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <FormLabel>名前</FormLabel>
-        <FormInput onChangeText={text => console.log(text)} />
+        <FormInput
+          onChangeText={name => this.setState({ name })}
+          value={this.state.name}
+        />
         <FormLabel>カロリー(kcal)</FormLabel>
         <FormInput
-          onChangeText={text => console.log(text)}
+          onChangeText={cal => this.setState({ cal: Number(cal) })}
           keyboardType={"number-pad"}
+          value={this.state.cal.toString()}
         />
         <FormLabel>タンパク質(g)</FormLabel>
         <FormInput
-          onChangeText={text => console.log(text)}
+          onChangeText={protein => this.setState({ protein: Number(protein) })}
           keyboardType={"number-pad"}
+          value={this.state.protein.toString()}
         />
         <FormLabel>脂質(g)</FormLabel>
         <FormInput
-          onChangeText={text => console.log(text)}
+          onChangeText={lipid => this.setState({ lipid: Number(lipid) })}
           keyboardType={"number-pad"}
+          value={this.state.lipid.toString()}
         />
         <FormLabel>炭水化物(g)</FormLabel>
         <FormInput
-          onChangeText={text => console.log(text)}
+          onChangeText={carbohydrate =>
+            this.setState({ carbohydrate: Number(carbohydrate) })
+          }
           keyboardType={"number-pad"}
+          value={this.state.carbohydrate.toString()}
         />
+        <Button title="登録" onPress={() => this.createFood()} />
       </View>
     );
   }
---
+++ utils/Fire.js
@@ -0,0 +1,60 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+class Fire {
+  constructor() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBzdWCw3BoZ0iRpXpdlof_-r3UfBN-xvhI",
+      authDomain: "food-note.firebaseapp.com",
+      databaseURL: "https://food-note.firebaseio.com",
+      projectId: "food-note",
+      storageBucket: "food-note.appspot.com",
+      messagingSenderId: "450257548170"
+    });
+    firebase.firestore().settings({ timestampsInSnapshots: true });
+
+    firebase.auth().onAuthStateChanged(async user => {
+      if (!user) {
+        await firebase.auth().signInAnonymously();
+      }
+    });
+  }
+
+  createFood = ({ name, cal, protein, lipid, carbohydrate, date }) => {
+    const createdAt = Date.now();
+    this.foodCollection.add({
+      name,
+      cal,
+      protein,
+      lipid,
+      carbohydrate,
+      date,
+      createdAt
+    });
+  };
+
+  getFoods = async () => {
+    const querySnapshot = await this.foodCollection.get();
+    const res = [];
+    querySnapshot.forEach(doc => {
+      res.push(doc.data());
+    });
+    return res;
+  };
+
+  // Helpers
+  get userCollection() {
+    return firebase.firestore().collection("users");
+  }
+
+  get foodCollection() {
+    return this.userCollection.doc(this.uid).collection("foods");
+  }
+
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+}
+
+Fire.shared = new Fire();
+export default Fire;
